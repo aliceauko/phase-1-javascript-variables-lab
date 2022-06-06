44dc8f09bf91c43c8419f8e6783e26450ada721a
aliceauko <alice.auko@student.moringaschool.com>
2022-06-07 01:21:51 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+const companyName = "Scuber";
+
+let mostProfitableNeighborhood = 'Chelsea'
+mostProfitableNeighborhood;
+let companyCeo = 'Susan Smith'
+companyCeo;
